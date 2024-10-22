b940220105852640c727967d7370a4b4c49ebca5
BOGERA-GIF <bogerahenry333@gmail.com>
2024-10-22 16:55:02 +0300

    Using npm Package of sillyname

--- using node/index.js
+++ using node/index.js
@@ -1,7 +1,20 @@
 // console.log("Hello world!");
 const fs = require("fs");
 
-fs.writeFile("message.txt", "Hello from NodeJS!" , (err) => {
-    if (err) throw err;
-    console.log("File has been saved!");
-});
+// fs.writeFile("message.txt", "Hello from NodeJS!" , (err) => {
+//     if (err) throw err;
+//     console.log("File has been saved!");
+// });
+
+// fs.readFile("message.txt" , (err) => {
+//     if (err) throw err;
+//     console.log("File has been saved!");
+// });
+// fs.readFile('message.txt', 'utf8', (err, data) => {
+//     if (err) {
+//         console.error("Error reading the file:", err);
+//         return;
+//     }
+//     console.log("File content:", data);
+// });
+
